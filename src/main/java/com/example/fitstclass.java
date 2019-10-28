698f95c1065608dc52bfd6d90407771d636c25c0
zheng <1071877529>
2019-10-28 19:26:50 +0800

    Initial commit

---
+++ src/main/java/com/example/fitstclass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @Author: zheng
+ * @Data :2019/10/28  19:15
+ */
+public class fitstclass {
+    public  static  void main(String[] arg)
+    {
+        System.out.println("htllo");
+    }
+}
